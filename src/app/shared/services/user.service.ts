e0eae13b1993e0f87e8578e41205fd918440f9cb
Rerimoi <prkwonyike@gmail.com>
2019-02-05 17:27:52 +0300

    module edits

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
-import { Observable } from "rxjs/Observable";
 import { User, Roles } from "../models/user";
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -15,13 +14,13 @@ export class UserService {
   // roles: Observable<Roles[]>;
 
   constructor(private firestore: AngularFirestore) {
-    this.user = this.firestore.collection<User>('user').snapshotChanges().pipe(
+    // this.user = this.firestore.collection<User>('user').snapshotChanges().pipe(
       // map(actions => actions.map(a => {
       //   const data = a.payload.doc.data() as User;
       //   const id = a.payload.doc.id;
       //   return { id, ...data };
       // }))
-    )
+    // )
   }
 
    getUser(){
