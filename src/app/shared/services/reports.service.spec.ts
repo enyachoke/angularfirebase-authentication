911b773a8ebdd8cc6936d5fde7afe80b95f1c96f
Mwende <josefridahmwende71@gmail.com>
2019-02-05 13:37:31 +0300

    report

--- src/app/shared/services/reports.service.spec.ts
+++ src/app/shared/services/reports.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ReportService } from './report.service';
+import { ReportsService } from './reports.service';
 
-describe('ReportService', () => {
+describe('ReportsService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: ReportService = TestBed.get(ReportService);
+    const service: ReportsService = TestBed.get(ReportsService);
     expect(service).toBeTruthy();
   });
 });
--- src/app/shared/services/reports.service.ts
+++ src/app/shared/services/reports.service.ts
@@ -3,8 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class ReportService {
+export class ReportsService {
 
   constructor() { }
-
 }
