0892aa327b597a4adb2a4eac2f812ec1a0d106c3
Pretorious Ndung'u <pretoriousndungu@gmail.com>
2019-02-05 06:02:24 +0300

    imported user profile

--- src/app/shared/routing/app-routing.module.ts
+++ src/app/shared/routing/app-routing.module.ts
@@ -7,6 +7,7 @@ import { SignUpComponent } from '../../components/sign-up/sign-up.component';
 import { DashboardComponent } from '../../components/dashboard/dashboard.component';
 import { ForgotPasswordComponent } from '../../components/forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from '../../components/verify-email/verify-email.component';
+import { UserProfileComponent } from '../../components/user-profile/user-profile.component';
 
 // Import canActivate guard services
 import { AuthGuard } from '../../shared/guard/auth.guard';
@@ -17,6 +18,7 @@ import { EditLecturerCheckinComponent } from '../../components/edit-lecturer-che
 import { LecturerCheckinListComponent } from '../../components/lecturer-checkin-list/lecturer-checkin-list.component';
 import { LecturerCheckinViewComponent } from 'src/app/components/lecturer-checkin-view/lecturer-checkin-view.component';
 import { LogInComponent } from 'src/app/components/log-in/log-in.component';
+import { from } from 'rxjs';
 
 // Include route guard in routes array
 const routes: Routes = [
@@ -25,12 +27,14 @@ const routes: Routes = [
   {path:  'log-in', component:LogInComponent, canActivate:[SecureInnerPagesGuard],},
   { path: 'register-user', component: SignUpComponent, canActivate: [SecureInnerPagesGuard]},
   { path: 'dashboard', component: DashboardComponent, canActivate: [SecureInnerPagesGuard] },
+  { path: 'user-profile', component: UserProfileComponent, canActivate: [SecureInnerPagesGuard] },
   { path: 'forgot-password', component: ForgotPasswordComponent, canActivate: [SecureInnerPagesGuard] },
   { path: 'verify-email-address', component: VerifyEmailComponent, canActivate: [SecureInnerPagesGuard] },
   { path: 'add-lecturer-checkin', component: AddLecturerCheckinComponent, canActivate: [SecureInnerPagesGuard] },
   { path: 'edit-lecturer-checkin/:id', component: EditLecturerCheckinComponent, canActivate: [SecureInnerPagesGuard] },
   { path: 'view-lecturer-checkin/:id', component: LecturerCheckinViewComponent, canActivate: [SecureInnerPagesGuard] },
-  { path: 'view-lecturer-checkins', component: LecturerCheckinListComponent, canActivate: [SecureInnerPagesGuard] }
+  { path: 'view-lecturer-checkins', component: LecturerCheckinListComponent, canActivate: [SecureInnerPagesGuard] },
+  {path: '**', component: DashboardComponent, canActivate: [SecureInnerPagesGuard] }  // If no matching route found, go back to dashboard route
 ];
 
 @NgModule({
